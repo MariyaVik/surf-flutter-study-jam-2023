b11d700d25a24eab64c51251bf742afa1b388cb9
Mariya Pelageicheva <pelagei4eva@gmail.com>
2023-04-09 21:44:59 +0500

    fixed

--- lib/features/ticket_storage/ticket_storage_page.dart
+++ lib/features/ticket_storage/ticket_storage_page.dart
@@ -27,41 +27,38 @@ class TicketStoragePage extends StatelessWidget {
                   return TicketCard(ticket: state.filtred[index]);
                 }),
       ),
-      floatingActionButton: _controller.position.pixels ==
-              _controller.position.maxScrollExtent
-          ? null
-          : FloatingActionButton.extended(
-              onPressed: () {
-                showModalBottomSheet(
-                    isScrollControlled: true,
-                    shape: const RoundedRectangleBorder(
-                        borderRadius:
-                            BorderRadius.vertical(top: Radius.circular(20))),
-                    context: context,
-                    builder: (context) {
-                      return Padding(
-                        padding: EdgeInsets.only(
-                            bottom: MediaQuery.of(context).viewInsets.bottom),
-                        child: SizedBox(
-                          height: 200,
-                          child: Column(
-                            children: [
-                              Container(
-                                margin: const EdgeInsets.all(12),
-                                height: 4,
-                                width: 40,
-                                decoration: BoxDecoration(
-                                    color: Colors.grey,
-                                    borderRadius: BorderRadius.circular(5)),
-                              ),
-                              const Expanded(child: BottomSheetContent()),
-                            ],
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            showModalBottomSheet(
+                isScrollControlled: true,
+                shape: const RoundedRectangleBorder(
+                    borderRadius:
+                        BorderRadius.vertical(top: Radius.circular(20))),
+                context: context,
+                builder: (context) {
+                  return Padding(
+                    padding: EdgeInsets.only(
+                        bottom: MediaQuery.of(context).viewInsets.bottom),
+                    child: SizedBox(
+                      height: 200,
+                      child: Column(
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.all(12),
+                            height: 4,
+                            width: 40,
+                            decoration: BoxDecoration(
+                                color: Colors.grey,
+                                borderRadius: BorderRadius.circular(5)),
                           ),
-                        ),
-                      );
-                    });
-              },
-              label: const Text('Добавить')),
+                          const Expanded(child: BottomSheetContent()),
+                        ],
+                      ),
+                    ),
+                  );
+                });
+          },
+          label: const Text('Добавить')),
     );
   }
 }
